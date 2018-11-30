82f2a3aeaa00fa70aed83fe93bfc1245f76b39ad
vrojo <vin.rojo@gmail.com>
2018-11-30 11:11:12 +0100

    First test created to test the decreasing of quality when a day passes

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,9 @@
 package com.gildedrose;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.*;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class GildedRoseTest {
@@ -10,7 +13,19 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    public void should_decrease_the_quality_value_of_a_normal_object_by_one_for_one_day() {
+        String itemName = "+5 Dexterity Vest";
+        int itemSellIn = 10;
+        int itemQuality = 20;
+        Item[] items = new Item[] { new Item(itemName, itemSellIn, itemQuality) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality).isEqualTo(itemQuality - 1);
+    }
 }
