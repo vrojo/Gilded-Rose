5ad28e1fadcda521b167383e918a86e4d5c96dbb
vrojo <vin.rojo@gmail.com>
2018-11-30 11:31:50 +0100

    should increase the quality value of an Aged Brie when a day passes

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,15 +7,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class GildedRoseTest {
-
-    @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-    }
-
     @Test
     public void should_decrease_the_quality_value_of_a_normal_object_by_one_for_one_day() {
         String itemName = "+5 Dexterity Vest";
@@ -53,5 +44,18 @@ public class GildedRoseTest {
         app.updateQuality();
 
         assertThat(app.items[0].quality).isEqualTo(itemQuality - 2);
+    }
+
+    @Test
+    public void should_increase_the_quality_value_of_an_Aged_Brie_when_a_day_passes() {
+        String itemName = "Aged Brie";
+        int itemSellIn = 5;
+        int itemQuality = 20;
+        Item[] items = new Item[] { new Item(itemName, itemSellIn, itemQuality) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality).isEqualTo(itemQuality + 1);
     }
 }
